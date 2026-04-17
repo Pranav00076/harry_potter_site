5f72ade444d9ca710f0076b1608791924b58f786
Arya Chadha <aryachadha13@gmail.com>
2026-04-18 01:59:49 +0530

    Flims page added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import Homepage from './components/homepage/Homepage'
 
 import './App.css'
+import Films from './components/Films/Flims'
 
 function App() {
 
   return (
     <>
       <Homepage />
+      {/* <Films/> */}
     </>
   )
 }
