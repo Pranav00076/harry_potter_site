ecedf94cda7b000da92fd9bc579c0159a5047002
Arya Chadha <aryachadha13@gmail.com>
2026-04-18 00:55:10 +0530

    Revert "Develop"

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
-import Homepage from './components/homepage/Homepage'
 
 import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Homepage />
+      
     </>
   )
 }
